7424e14bdbf78a64804db9f80fe379159ffdb47c
Dab <SecondKoowars@gmail.com>
2017-02-08 19:52:22 -0800

    Fixed the installer, it wasnt installing this ver

--- mod-files/src/installer.js
+++ mod-files/src/installer.js
@@ -1,5 +1,5 @@
-ScratchExtensions.loadExternalJS('https://rawgit.com/ScratchOs/Scratch-Mod/master/src/misc.js');
-ScratchExtensions.loadExternalJS('https://rawgit.com/ScratchOs/Scratch-Mod/master/src/math.js');
-ScratchExtensions.loadExternalJS('https://rawgit.com/ScratchOs/Scratch-Mod/master/src/logic.js');
-ScratchExtensions.loadExternalJS('https://rawgit.com/ScratchOs/Scratch-Mod/master/src/html5local.js');
-ScratchExtensions.loadExternalJS('https://rawgit.com/ScratchOs/Scratch-Mod/master/src/script.js');
+ScratchExtensions.loadExternalJS('https://rawgit.com/LittleK00PA/Scratch/master/mod-files/src/misc.js');
+ScratchExtensions.loadExternalJS('https://rawgit.com/LittleK00PA/Scratch/master/mod-files/src/math.js');
+ScratchExtensions.loadExternalJS('https://rawgit.com/LittleK00PA/Scratch/master/mod-files/logic.js');
+ScratchExtensions.loadExternalJS('https://rawgit.com/LittleK00PA/Scratch/master/mod-files/src/html5local.js');
+ScratchExtensions.loadExternalJS('https://rawgit.com/LittleK00PA/Scratch-Mod/master/mod-files/src/script.js');
